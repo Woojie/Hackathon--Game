3c7a1d965646a409e19cf121ab8cf71c45c71cd1
Wooj <wjchung@hotmail.ca>
2018-11-08 14:20:31 -0800

    Combined Validate

--- scrabble/src/App.js
+++ scrabble/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from'./Components/Board'
 import Validate from './Components/Validate'
+import { Header } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
--- scrabble/src/Components/Board.js
+++ scrabble/src/Components/Board.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react'
 import { Form, Button, Header } from 'semantic-ui-react'
-
+import Validate from './Validate'
+import axios from 'axios'
 
 class Board extends Component {
   state = {
-    text: ''
-  }
-  textInput = e => {
-    this.setState({
-      text:e.target.value
-    })
+    wordExist: '',
+    searchText: ''
   }
 
+
+  getSearch = e => {
+    this.setState({
+        searchText: e.target.value
+    })
+}
+
+search = (searchText) => {
+
+  const url = 'http://api.urbandictionary.com/v0/define?term='
+  axios.get(url + searchText)
+  .then (res => {
+      let data = res.data.list
+      this.setState({
+         wordExist:  data.length > 0 ? 'Yes' : 'No'
+      })
+  })
+}
+
   render(){
-    
     return(
       <div>
       <Form>
         <Form.Field>
           <label>Write your word down!</label>
-          <input type='text'  onChange={this.textInput} />
-          <Button type='submit'>Submit</Button>
+          <input type='text'  onChange={this.getSearch} />
 
         </Form.Field>
+        <Button type='submit' onClick={() =>this.search(this.state.searchText)}>Click</Button>
       </Form>
   
       <Header as='h1'>{this.state.text}</Header>
+      Does the word exist? {this.state.wordExist}
       </div>
     )
   }
