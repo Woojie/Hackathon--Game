b409ee9fb61f5aa6db8203a37c897fc26b134306
Wooj <wjchung@hotmail.ca>
2018-11-09 11:27:23 -0800

    Added fun colors

---
+++ scrabble/src/Components/EndGame.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Header, Form, Button, Modal, Divider, Icon, Segment} from 'semantic-ui-react'
+import axios from 'axios'
+
+class EndGame extends Component {
+
+  render() {
+
+    return(
+      <div>Hello World</div>
+    )
+  }
+
+}
+export default EndGame
--- scrabble/src/Components/PlayerOneTiles.js
+++ scrabble/src/Components/PlayerOneTiles.js
@@ -8,16 +8,16 @@ render(){
 let player = []
 if(this.props.currentPlayer === 'player1'){
     player = this.props.player1.map((tiles, i ) => (    
-    <Popup inverted trigger={<Button circular key={i} onClick={() =>this.props.addText(tiles.name, tiles.value, i)} content={tiles.name}/>} ><Header as='h3'>{tiles.name}</Header><p>Value: <b>{tiles.value}</b></p></Popup>
+    <Popup inverted trigger={<Button   onClick={() =>this.props.addText(tiles.name, tiles.value, i)} content={tiles.name}/>} ><Header as='h3'>{tiles.name}</Header><p>Value: <b>{tiles.value}</b></p></Popup>
 
 ))
 }else{
     player = this.props.player2.map((tiles, i ) => (    
-        <Popup inverted trigger={<Button circular key={i} onClick={() =>this.props.addText(tiles.name, tiles.value, i)} content={tiles.name}/>} ><Header as='h3'>{tiles.name}</Header><p>Value: <b>{tiles.value}</b></p></Popup>
+        <Popup inverted trigger={<Button key={tiles.value + i} onClick={() =>this.props.addText(tiles.name, tiles.value, i)} content={tiles.name}/>} ><Header as='h3'>{tiles.name}</Header><p>Value: <b>{tiles.value}</b></p></Popup>
     ))}
 
 let newWord = this.props.textResult.map((tiles, i)=>(
-    <Popup inverted trigger={<Button circular key={i} onClick={() =>this.props.deleteText(tiles.name, tiles.value, i)} content={tiles.name}/>} ><Header as='h3'>{tiles.name}</Header><p>Value: <b>{tiles.value}</b></p></Popup>
+    <Popup inverted trigger={<Button key={tiles.value + i} onClick={() =>this.props.deleteText(tiles.name, tiles.value, i)} content={tiles.name}/>} ><Header as='h3'>{tiles.name}</Header><p>Value: <b>{tiles.value}</b></p></Popup>
 ))
     return (
         <div>
